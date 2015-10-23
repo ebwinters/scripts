c86a3891af9b267f4889275d92ca2117d6715106
ebwinters <Ethan@Ethan-Winters-Computer-2.local>
2015-10-22 22:58:43 -0500

    Player and dealer have cards dealt to them. Dealer only shows one card

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -29,6 +29,7 @@ class Card:
 	def get_rank(self):
 		return self.value
 
+
 class Deck:
 	def __init__(self):
 		self.deck = []
@@ -41,13 +42,61 @@ class Deck:
 		while counter < len(self.deck):
 			counter += 1
 
-	def shuffle(self, card_list):
-		shuffle(card_list)
-		return( card_list)
-	def deal_card(self, card_list):
-		dealt = card_list.pop()
-		return(dealt.value, dealt.suit)
-		
-deck = Deck()
-deck.shuffle(deck.deck)
-deck.deal_card(deck.deck)
+	def shuffle(self):
+		shuffle(self.deck)
+		return(self.deck)
+	def deal_card(self):
+		dealt = self.deck.pop()
+		return([dealt.value, dealt.suit])
+	
+
+class Player:
+	def __init__(self):
+		self.number = 0
+	def get_number(self):
+		return self.number
+class Dealer:
+	def __init__(self):
+		self.number = 0
+		self.numbershowing = 0
+	def get_number(self):
+		return self.number + self.numbershowing
+	def get_number_showing(self):
+		return self.numbershowing
+
+class Game:
+	deck = Deck()
+	deck.shuffle()
+	player = Player()
+	dealer = Dealer()
+	player_starting_cards = (deck.deal_card(), deck.deal_card())
+	dealer_starting_cards = (deck.deal_card(), deck.deal_card())
+	print( player_starting_cards)
+	print( dealer_starting_cards)
+	if player_starting_cards[0][0] and player_starting_cards[1][0] in VALUES:
+		print(VALUES[player_starting_cards[0][0]])
+		print(VALUES[player_starting_cards[1][0]])
+		player.number += VALUES[player_starting_cards[0][0]] + VALUES[player_starting_cards[1][0]]
+		print(player.number)
+	if dealer_starting_cards[0][0] in VALUES:
+		dealer.numbershowing = dealer_starting_cards[0][0]
+		dealer.number += VALUES[dealer_starting_cards[0][0]]
+		print(dealer.numbershowing)
+	if dealer_starting_cards[1][0] in VALUES:
+		dealer.number += VALUES[dealer_starting_cards[1][0]]
+		print(dealer.number)
+	
+
+
+
+
+	def hit(self, player):
+		pass
+	def stay(self, player):
+		pass
+
+	
+	#create dealer and player
+	#deal player, dealer
+	#ask for hit or not 
+Game()
