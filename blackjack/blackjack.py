041b0ab375894afd136bfda67849b9801e984458
ebwinters <Ethan@Ethan-Winters-Computer-2.local>
2015-10-26 18:46:25 -0500

    Betting somewhat words, need to fix the player bust

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -58,10 +58,9 @@ class Player:
 		self.identity = "p"
 	def get_number(self):
 		return self.number
-	def hit(self, deck):
+	def hit(self, deck, turn):
 		if self.number > 21:
 			print("BUST")
-			exit()
 		holder_list = (deck.deal_card(),)
 		# if holder_list[0][0] == 'A':
 		# 	oneoreleven = str(input(('Do you want the ace to be played as a value of 1 or 11 [1/11]')))
@@ -70,6 +69,7 @@ class Player:
 		# 	else:
 		# 		self.number += 11
 		print("The card you have been dealt is: ")
+		print(holder_list[0])
 		if holder_list[0][0] == 'A':
 			val = str(input("Do you want the ace to be worth 1 or 11? "))
 			if val == '1':
@@ -79,10 +79,11 @@ class Player:
 		else:
 			self.number += VALUES[holder_list[0][0]]
 
-		print(holder_list[0])
+		
 		if self.number > 21:
 			print("BUST")
-			exit()
+			turn = False
+
 		print("You are currently at a total of " + str(self.number))
 
 class Dealer:
@@ -97,92 +98,116 @@ class Dealer:
 	def hit(self, deck):
 		if self.number > 21:
 			print("BUST")
-			exit()
 		holder_list = (deck.deal_card(),)
 		self.number += VALUES[holder_list[0][0]]
 		print("The card the dealer was dealt is: ")
 		print(holder_list[0])
 		if self.number > 21:
 			print("DEALER BUST, YOU WIN")
-			exit()
-		print("The dealer is currently at a total of currently at a todal of " + str(self.number))
+		print("The dealer is currently at a total " + str(self.number))
 
 class Game:
 	os.system('clear')
 	print("=*="*20)
 	print("Welcome to Blackjack")
+	print("You'll start with 50 chips")
 	print("=*="*20)
-	deck = Deck()
-	deck.shuffle()
-	player = Player()
-	dealer = Dealer()
-	playerturn = True
-	dealerturn = False
-	player_starting_cards = (deck.deal_card(), deck.deal_card())
-	dealer_starting_cards = (deck.deal_card(), deck.deal_card())
-	# print( player_starting_cards)
-	# print( dealer_starting_cards)
-	if player_starting_cards[0][0] and player_starting_cards[1][0] in VALUES:
-		print("Here are your cards: ")
-		print(player_starting_cards)
-		if player_starting_cards[0][0] == 'A':
-			val = str(input("Do you want the ace to be worth 1 or 11? "))
-			if val == '1':
-				player.number += 1
-			if val == '11':
-				player.number += 11
-			player.number += VALUES[player_starting_cards[1][0]]
-		elif player_starting_cards[1][0] == 'A':
-			val = str(input("Do you want the ace to be worth 1 or 11? "))
-			if val == '1':
-				player.number += 1
-			if val == '11':
-				player.number += 11
-			player.number += VALUES[player_starting_cards[0][0]]
+	chips = 50
+	while chips > 0:
+		print("You currently have " + str(chips) + " chips")
+		deck = Deck()
+		deck.shuffle()
+		player = Player()
+		dealer = Dealer()
+		playerturn = True
+		dealerturn = False
+		player_starting_cards = (deck.deal_card(), deck.deal_card())
+		dealer_starting_cards = (deck.deal_card(), deck.deal_card())
+		chipval = input("How many chips do you wish to bet on this hand?: ")
+		if chipval:
+			print("Bet has been placed")
+		print('\n\n')
+		# print( player_starting_cards)
+		# print( dealer_starting_cards)
+		if player_starting_cards[0][0] and player_starting_cards[1][0] in VALUES:
+			print("Here are your cards: ")
+			print(player_starting_cards)
+			if player_starting_cards[0][0] == 'A':
+				val = str(input("Do you want the ace to be worth 1 or 11? "))
+				if val == '1':
+					player.number += 1
+				if val == '11':
+					player.number += 11
+				player.number += VALUES[player_starting_cards[1][0]]
+			elif player_starting_cards[1][0] == 'A':
+				val = str(input("Do you want the ace to be worth 1 or 11? "))
+				if val == '1':
+					player.number += 1
+				if val == '11':
+					player.number += 11
+				player.number += VALUES[player_starting_cards[0][0]]
 
-		else:
-			player.number += VALUES[player_starting_cards[0][0]] + VALUES[player_starting_cards[1][0]]
-		print("You are currently at a total of " + str(player.number))
-	if dealer_starting_cards[0][0] in VALUES:
-		dealer.numbershowing = dealer_starting_cards[0]
-		dealer.number += VALUES[dealer_starting_cards[0][0]]
-		print("Here is what the dealer has showing: ")
-		print(dealer.numbershowing)
-	if dealer_starting_cards[1][0] in VALUES:
-		dealer.number += VALUES[dealer_starting_cards[1][0]]
-	while playerturn == True:
-		hit_or_stay = input("Would you like to hit or stay? [h/s]").lower()
-		if player.number <= 21:
-			if hit_or_stay == 'h':
-				player.hit(deck)
-			if hit_or_stay == 's':
-				playerturn = False
-				dealerturn = True
-		elif player.number > 21:
-			print("BUST")
-			exit()
-	while dealerturn == True:
-		print("\n")
-		print("The dealer has flipped the card facing down, revealing the: ")
-		print(dealer_starting_cards[1])
-		print("The dealers total is: " + str(dealer.number))
-		while dealerturn == True:	
-			stay_list = [17, 18, 19, 20, 21]
-			if dealer.number <= 16:
-				print("Dealer has to hit")
-				dealer.hit(deck)
-			if dealer.number in stay_list:
+			else:
+				player.number += VALUES[player_starting_cards[0][0]] + VALUES[player_starting_cards[1][0]]
+			print("You are currently at a total of " + str(player.number))
+		if dealer_starting_cards[0][0] in VALUES:
+			dealer.numbershowing = dealer_starting_cards[0]
+			dealer.number += VALUES[dealer_starting_cards[0][0]]
+			print("Here is what the dealer has showing: ")
+			print(dealer.numbershowing)
+		if dealer_starting_cards[1][0] in VALUES:
+			dealer.number += VALUES[dealer_starting_cards[1][0]]
+		while playerturn == True:
+			if player.number < 22:
+				hit_or_stay = input("Would you like to hit or stay? [h/s]").lower()
+				if player.number <= 21:
+					if hit_or_stay == 'h':
+						player.hit(deck, playerturn)
+					if hit_or_stay == 's':
+						playerturn = False
+						dealerturn = True
+				elif player.number > 21:
+					print("BUST")
+					break
+		while dealerturn == True:
+			print("\n")
+			print("The dealer has flipped the card facing down, revealing the: ")
+			print(dealer_starting_cards[1])
+			print("The dealers total is: " + str(dealer.number))
+			while dealerturn == True:	
+				stay_list = [17, 18, 19, 20, 21]
+				if dealer.number <= 16:
+					print("Dealer has to hit")
+					dealer.hit(deck)
+				if dealer.number in stay_list:
+					dealerturn = False
+					playerturn = True
+					print('\n\n')
+					print("Dealer stays")
+				elif dealer.number > 21:
+					print("DEALER BUST, YOU WIN")
+					chips += int(chipval)
+					dealerturn = False
+					playerturn = True
+					print('\n\n')
+			if dealer.number >= player.number:
+				print("Dealer wins")
+				chips -= int(chipval)
 				dealerturn = False
-				print("Dealer stays")
-			elif dealer.number > 21:
-				print("DEALER BUST, YOU WIN")
-				exit()
-		if dealer.number >= player.number:
-			print("Dealer wins")
-			exit()
-		else:
-			print("You win")
-
+				playerturn = True
+				print('\n\n')
+			elif dealer.number == player.number:
+				print("Push")
+				dealerturn = False
+				playerturn = True
+				print('\n\n')
+			else:
+				print("You win")
+				chips += int(chipval)
+				dealerturn = False
+				playerturn = True
+				print('\n\n')
+	print("Game over")
 
 Game()
 # betting with chips
