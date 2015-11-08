c2d77fe0f871255e48be26230b7d21fd0d69de1a
ebwinters <Ethan@Ethan-Winters-Computer-2.local>
2015-11-08 11:46:45 -0600

    making application that finds a teacher's email from the school website and emails them your message

--- crawler/crawling_practice.py
+++ crawler/crawling_practice.py
@@ -2,9 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "http://www.yellowpages.com/search?search_terms=coffee&geo_location_terms=Los+Angeles%2C+CA"
+url = "http://hphs.dist113.org/Lists/EmployeeDirectory7/AllItems.aspx"
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content)
-#gets title
-print(soup.title.string)
+
+data = soup.find_all("div", {"class": "ms-vb itx"})
+
+
+teacher_list = []
+
+for item in data:
+	# listing = item.find_all('a')
+	# print(item.get("href"))
+	instance = item.a.string
+	num = instance.find(',')
+	new = instance[0:num]
+	teacher_list.append(new)
+
+teacher = input("What is the teacher's last name?: ")
+
+if teacher in teacher_list:
+	print("Y")
+else:
+	print("N")
+
+
