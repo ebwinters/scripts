828ce3ea352c9b0c6c2a7a2611ecb6d46c01baf1
ebwinters <Ethan@Ethan-Winters-Computer-2.local>
2015-11-26 16:53:37 -0600

    began derivative application

---
+++ derivatives/derivative.py
@@ -0,0 +1,13 @@
+def simple_derivative(polynomial):
+	print("hey")
+
+
+
+
+class Run:
+	selection = input("Press <1> for the derivative of a polynomial: ")
+	if selection == 1:
+		polynomial = str(input("Please input the polynomial: "))
+		print (simple_derivative(polynomial))
+
+Run()
