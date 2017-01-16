e96b0a4ecdec19357ae5054b300b499a19c15ccb
Ethan Winters <ewinters@terpmail.umd.edu>
2017-01-15 20:44:22 -0600

    changed topic to bodybuilding.com so making prep to scrape

--- dataOrg/backend.py
+++ dataOrg/backend.py
@@ -1,18 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-# def getHotStocks(self):
+exersizes = {'neck':6,'traps':11,'shoulders':12,'chest':1,'biceps':15,'forearms':2,'abdominals':13,
+'quadriceps':7,'calves':9,'triceps':10,'lats':3,'middle-back':4,'lower-back':5,'glutes':14,'hamstrings':8}
+
+# def getExercizes(self):
+for k,v in 
 url = "http://money.cnn.com/data/hotstocks/index.html"
 r = requests.get(url)
 soup = BeautifulSoup(r.content, "html.parser")
 
-# g_data = soup.find_all("tr")
 
-# for item in g_data:
-# 	link_data = item.find_all("span")
-# 	print (link_data)
-# 	print ("\n\n")
 
-g_data = soup.find_all("table", {"class":"wsod_dataTable wsod_dataTableBigAlt"})
-#gets only the most active hotstocks
-print (g_data[0])
+
+
